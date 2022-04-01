5bcfdda1ef43dbf4281bc206006a9a174ed5e74d
Gregoriotb <jgregoriotbaltar@gmail.com>
2022-04-01 16:31:22 -0400

    terminado el segundo taller de js, hay que meterle maquetacion y listo

--- descuentos.js
+++ descuentos.js
@@ -1,4 +1,24 @@
-const precioOriginal = 150;
-const descuento = 25;
+// const precioOriginal = 150;
+// const descuento = 15;
 
-const porcentajePrecioConDescuento = 100 - descuento;
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function botonDescuento () {
+    const calculadorPrecio = document.getElementById ("InputPrecio")
+    const calculoprecio = calculadorPrecio.value;
+
+    const calculadorDescuento = document.getElementById ("InputDescuento")
+    const calculoDescuento = calculadorDescuento.value;
+
+    const precioConDescuento = "Tu nuevo precio sera $" + calcularPrecioConDescuento (calculoprecio, calculoDescuento)
+    
+    const resultado = document.getElementById("Resultado")
+    resultado.innerText = precioConDescuento
+}
+// const porcentajePrecioConDescuento = 100 - descuento;
+// const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100;
